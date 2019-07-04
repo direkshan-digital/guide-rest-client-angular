9e7e61361c2a35e64575214a58ae884b4fab88f8
Prafull Kotecha <Prafull.Kotecha@gmail.com>
2019-07-03 21:26:07 -0400

    add material design with bootstrap

--- start/src/main/frontend/src/app/app.component.ts
+++ start/src/main/frontend/src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, Injectable, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
+@Injectable()
+export class ArtistsService {
+  constructor(private http: HttpClient) { } //DevSkim: ignore DS137138 
+
+  private static ARTISTS_URL = '/artists';
+
+  async fetchArtists() {
+    try {
+      const data: any = await this.http.get(ArtistsService.ARTISTS_URL).toPromise();
+      return data;
+    } catch (error) {
+      console.error(`Error Occurred: ${error}`);
+    }
+  }
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  providers: [ArtistsService],
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit {
+  artists: any[] = [];
+  constructor(private artistsService: ArtistsService) {}
+  ngOnInit() {
+    this.artistsService.fetchArtists().then(data => {
+      this.artists = data;
+    });
+  }
 }
--- start/src/main/frontend/src/app/app.module.ts
+++ start/src/main/frontend/src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    MDBBootstrapModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
